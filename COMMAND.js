df10148995dc0041ca212b82a97998bdfa88c81b
RIFAQAT6244 <143928773+RIFAQAT6244@users.noreply.github.com>
2023-09-11 15:49:49 +0600

    Add files via upload

---
+++ COMMAND.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Get a reference to the button
+const startButton = document.getElementById('start-button');
+
+// Add a click event listener to the button
+startButton.addEventListener('click', () => {
+    // Redirect to index.html
+    window.location.href = 'tic tac toe.html';
+});
